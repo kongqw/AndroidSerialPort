c4e42b8b0c93cc5058c2239de39cee078293f4e5
kongqw <qingwei.kong@bitmaintech.com>
2016-10-31 18:59:39 +0800

    添加Readme

--- app/src/main/java/qingwei/kong/kqwserialportdemo/MyApplication.java
+++ app/src/main/java/qingwei/kong/kqwserialportdemo/MyApplication.java
@@ -22,25 +22,6 @@ public class MyApplication extends Application {
 
     public SerialPort getSerialPort() throws SecurityException, IOException, InvalidParameterException {
         if (mSerialPort == null) {
-//            /* Read serial port parameters */
-//            SharedPreferences sp = getSharedPreferences(getPackageName(), MODE_PRIVATE);
-//            String path = sp.getString("DEVICE", "");
-//            int baudrate = Integer.decode(sp.getString("BAUDRATE", "-1"));
-//            String path = sp.getString("DEVICE", "/dev/ttyS2");
-//            int baudrate = Integer.decode(sp.getString("BAUDRATE", "115200"));
-//
-//            String path = "/dev/ttyS2";
-//            int baudrate = 115200;
-//
-//            Log.i(TAG, "getSerialPort: path = " + path + "   baudrate = " + baudrate);
-//            /* Check parameters */
-//            if ((path.length() == 0) || (baudrate == -1)) {
-//                throw new InvalidParameterException();
-//            }
-//
-//			/* Open the serial port */
-//            mSerialPort = new SerialPort(new File(path), baudrate, 0);
-
             mSerialPort = new SerialPort(new File("/dev/ttyS2"), 115200, 0);
         }
         return mSerialPort;
@@ -48,7 +29,7 @@ public class MyApplication extends Application {
 
     public void closeSerialPort() {
         if (mSerialPort != null) {
-//            mSerialPort.close();
+            mSerialPort.close();
             mSerialPort = null;
         }
     }
